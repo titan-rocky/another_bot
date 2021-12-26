a02454220a1fc9c9065dfea9b9a641f104856c76
titan-rocky <rockalbert466@gmail.com>
2021-12-26 19:21:42 +0530

    s

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ async def morning():
 	if b.startswith('00:30'):
 		lb=['Good Night Priends !','Mallaakka Paduthu Vittathai Paakkurathu Evlo Sugam , Thoonga poren zzz']
 		ddd=[i.status for i in gex.members]
-		mo=[i!=discord.Status.offline for i in ddd]
+		mo=[i!=discord.Status.offline and not(i.bot) for i in ddd]
 		bcount=mo.count(True)
 		print(bcount)
 		if bcount>5:
